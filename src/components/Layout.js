8e140ca74c13b5000cfe6c2368f092ade24dc94b
Mika Sormunen <mika@halian.fi>
2021-12-06 22:20:53 +0200

    Initial commit

---
+++ src/components/Layout.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+
+const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  }
+  return (
+    <>
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
